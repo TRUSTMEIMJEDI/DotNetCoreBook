da047acebad51ab35fdf9e7ca9afe8a4dea759a3
Marcin Friedrich <fredros1996@gmail.com>
2019-04-16 23:04:06 +0200

    Added BookRepository with Interface

---
+++ src/DotNetWebApiBook/Repository/BookRepository.cs
@@ -0,0 +1,36 @@
+﻿using DotNetWebApiBook.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotNetWebApiBook.Repository
+{
+    public class BookRepository : IBookRepository<Book>
+    {
+        public Task Add(Book entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Delete(Book book)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Book> Get(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Book>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Update(Book book, Book entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/DotNetWebApiBook/Repository/IBookRepository.cs
@@ -0,0 +1,21 @@
+﻿using DotNetWebApiBook.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotNetWebApiBook.Repository
+{
+    public interface IBookRepository<TEntity>
+    {
+        Task<IEnumerable<TEntity>> GetAll();
+
+        Task<TEntity> Get(long id);
+
+        Task Add(TEntity entity);
+
+        Task Update(Book book, TEntity entity);
+
+        Task Delete(Book book);
+    }
+}
